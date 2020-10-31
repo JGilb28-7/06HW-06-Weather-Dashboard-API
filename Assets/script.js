8f5a0b2ab831d52951c894fe148b1193649ad7ca
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-10-31 00:21:36 -0400

    API connecting and returning

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,23 @@
 //AJAX I Need the JSON to pull Weather which come from the index
-const cityName = "$city-input" //text
+//const cityName = $("#search-term").val() //text
+//const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ff72d96a24410b758f22678b53189672`
+//let citySearch = $("#search-term").val()
+
+//const cityName = $("#search-input").val() //text
+
+//$("add-city").on("click", function() {
+//const cityName = $(this).attr("text");
+const cityName = "boston"
 const queryURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=ff72d96a24410b758f22678b53189672`
 
-function updatePage(response){
-    console.log (response)
-
-}
-
-$.ajax({
+  function updatePage(response){
+  console.log(cityName)
+    }
+  $.ajax({
     url: queryURL,
     method: "GET"
-  }).then(updatePage) //promise
 
-
+  }).then(function(response) {
+      $("#cityNameView").text(JSON.stringify(response));
+  })
+//});
