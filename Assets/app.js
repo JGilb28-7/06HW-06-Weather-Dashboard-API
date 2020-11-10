52a191eb4904f7bfcc369d0d9f7d739afcaad569
JGilbe28-7 <jonathan.gilbert@mac.com>
2020-11-10 15:09:55 -0500

    added date, 5 day forecast correct, started UV

--- Assets/app.js
+++ Assets/app.js
@@ -9,11 +9,13 @@ const tempCont = $("#temp");
 const feelsLkCont = $("#feelsLk");
 const tempfCont = $("#tempF");
 const feelsLkFCont = $("#feelsLkF");
+const timeCont = $("#time");
 const uvIndxCont = $("#uvIndex")
 const submitBtn = $("#add-city");
 let cities = ["Boston" , "Lynn"];
-let forecastDiv = $("fiveDayForecast"); 
-const dateCont = $("date");
+let forecastDiv = $("fiveDayForecast");
+
+
 //to do - get local storage
 
 function convertToFerinheight(temp) {
@@ -22,7 +24,6 @@ function convertToFerinheight(temp) {
 }
 
 function convertdateTime(){
-
 
 }
 function getCurrentWeatherAndDisplay(cityName) {
@@ -40,9 +41,9 @@ function getCurrentWeatherAndDisplay(cityName) {
 
     //shows from the HTML that the details div is empty used the Sample from Bujumbura
 
-    cityTitle.html("<h2>" + response.name );
-    //dateCont.text("<h2>" + new Date().toLocaleDateString());
+    cityTitle.html("<h2>" + response.name + "- Current Weather");
     //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
+    timeCont.text("Time: " + new Date(response.dt * 1000));
     windSpeedCont.text("Wind Speed: " + response.wind.speed);
     humidityCont.text("Humidity: " + response.main.humidity);
    
@@ -53,8 +54,9 @@ function getCurrentWeatherAndDisplay(cityName) {
     // add temp content to html
     tempCont.text("Temperature (K) " + response.main.temp);
     feelsLkCont.text("Feel Like:  " + response.main.feels_like);
-    tempfCont.text("Temperature (F) " + tempF.toFixed(2));
-    feelsLkFCont.text("Feels Like (F) " + feelsLkF.toFixed(2));
+    tempfCont.text("Temperature (F) " + tempF.toFixed(0));
+    feelsLkFCont.text("Feels Like (F) " + feelsLkF.toFixed(0));
+    uvIndxCont.text("UV Index: " + response.coord.lat);
     //uvIndxCont.text("UV Index: " + response.weather);//need to link the Array and icon//
 
     // Log the data in the console as well
@@ -62,7 +64,7 @@ function getCurrentWeatherAndDisplay(cityName) {
     console.log("Humidity: " + response.main.humidity);
     console.log("Temperature (F): " + tempF);
     console.log("Feel Like:" + response.main.feels_like);
-    console.log("date:" + response.dt);
+    //console.log("date:" + response.dt);
   });
 }
 
@@ -84,42 +86,67 @@ function getFiveDayForcastAndDisplay(cityName) {
      
       //Stackoverflow resource: had to update with my code
       const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-      const timesToDisplay = [0, 8, 16, 32, 40, 48];
+      const timesToDisplay = [8, 16, 24, 31, 39];
       let d;
       let dayName;
       let weatherFive = '';
      
       // need to create a loop over the time not use the cnt that will limit what is displayed from the array of title:
-      weatherFive += "<h2>" + response.city.name + " Five Day Forcast </h2>"; // City (displays once)
+      weatherFive += "<h2>" + response.city.name + " Five Day Forecast </h2>"; // City (displays once)
       //weatherFive += "<b> Date: " + response.list.dt_txt + "</b>";//Date - need to adjust 
     
       // changed to respsone, updated the varibale weatherFive, add the date dt_txt but coming back undefined
       $.each(response.list, function(index, val) {
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration - line 17 as example.
-        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
-        //arr.indexOf(searchElement[, fromIndex])
-        //for (var i = 0; i < response.list.length; i++)
+      //see notes in .md for resources listed:
+       // for(i=0+8; i < response.list.length; i++){
         if(timesToDisplay.includes(index)){
-        d = new Date(response.list[index].dt * 1000);
+        d = new Date(response.list[index].dt * 1000); //jsg - convert to miliseconds
         dayName = days[d.getDay()];
 
         weatherFive += "<p> " // Opening paragraph tag
         weatherFive += "<b> " + index + "</b>: " // Day
         //added the convert to Ferinheight to line and the .toFixed() only working on Day 0.
         weatherFive += "<b>" + timesToDisplay.indexOf(index) + " (" + dayName + ")</b>: " // DateTime
-        weatherFive += convertToFerinheight(val.main.temp).toFixed(2) + "&degF" // Temperature
+        weatherFive += convertToFerinheight(val.main.temp).toFixed(0) + "&degF" // Temperature
         weatherFive += "<span> | Humidity: "+ val.main.humidity + "% </span>"
         weatherFive += "<span> | " + val.weather[0].description + "</span>"; // Description
         weatherFive += "<img src='https://openweathermap.org/img/w/" + val.weather[0].icon + ".png'>" // Icon
         weatherFive += "</p>" // Closing paragraph tag
         
         }
+      //} goes to loop
     });
       $("#fiveDayForecast").html(weatherFive);
 
   });
 }
 
+function getFiveUVIndexAndDisplay(cityName) {
+  //let lat = response.city.coord.lat;
+  //let lon = response.city.coord.lon;
+  
+  const queryURL = `http://api.openweathermap.org/data/2.5/uvi?lat=${"lat"}&lon=${"lon"}&appid=appid=ff72d96a24410b758f22678b53189672`;
+ 
+ $.ajax({
+      url: queryURL,
+      method: "GET",
+      
+    }).then(function (response) {
+      var uvI = response.value;
+      uvIndexDiv.text("UV Index: " + uvI);
+      console.log(queryURL);
+
+    // Log the resulting object  
+      console.log(response);
+       
+     
+    })
+    //} goes to loop
+  }
+    //$("#fiveDayForecast").html(weatherFive);
+
+//});
+//}
 //submit
 submitBtn.on("click", function (event) {
   event.preventDefault();
@@ -129,6 +156,7 @@ submitBtn.on("click", function (event) {
   console.log(cityName);
   getCurrentWeatherAndDisplay(cityName);
   getFiveDayForcastAndDisplay(cityName);
+  getFiveUVIndexAndDisplay(cityName);
 
   renderButtons();
 });
@@ -154,5 +182,6 @@ $(document).on("click", ".cityBtns", function(){
 console.log($(this).text())
 getCurrentWeatherAndDisplay($(this).text())
 getFiveDayForcastAndDisplay($(this).text())
+getFiveUVIndexAndDisplay($(this).text());
 });
 
--- README.md
+++ README.md
@@ -13,6 +13,35 @@ Used the [OpenWeather API](https://openweathermap.org/api) to retrieve weather d
 ## Tasks
 Optained the openweathermap key: ff72d96a24410b758f22678b53189672
 
+## Resources used:
+Google
+Openweatherapp.org - API Documentation
+Class activities - used the Movie references and 05-Bujumbura
+Used the cnt = 5 to limit the daily response - this is in the API Doc for Weather 
+
+https://stackoverflow.com/questions/49640174/building-a-5-day-forecast-using-open-weather-api-ajax-and-js
+https://sceendy.com/blog/2017/09-27-weather-widget-tutorial/
+
+looping over the dt_txt - tried to loop over see committs from 11/9/2020.
+https://stackoverflow.com/questions/62004236/nested-loops-looping-through-open-weather-api-and-classes-in-html
+
+Time in the Array for 5 day.
+https://stackoverflow.com/questions/56070796/show-day-name-instead-of-number-from-open-weather-api-response - needed to change the isoString to local 
+
+Loops: using the forIndex
+ https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration - line 17 as example.
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+arr.indexOf(searchElement[, fromIndex]) - example
+
+updating the array for 5 day:
+https://stackoverflow.com/questions/56070796/show-day-name-instead-of-number-from-open-weather-api-response
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration - line 17 as example.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
+example: arr.indexOf(searchElement[, fromIndex])
+
+
 ## Acceptance Criteria
 
 ```
@@ -21,8 +50,8 @@ WHEN I search for a city - complete
 THEN I am presented with current and future conditions for that city and that city is added to the search history - complete
 WHEN I view current weather conditions for that city - complete 
 
-city name
-the date
+city name - complete
+the date - 
 an icon representation of weather conditions - outstandind
 the temperature - complete
 the humidity - complete
@@ -30,7 +59,7 @@ the wind speed - complete
 the UV index - outstanding
     presented with a color that indicates whether the conditions are favorable, moderate, or severe
 
-view future weather conditions for that city - 5 day complete - 80%
+view future weather conditions for that city - 5 day complete - 100%
 I am presented with a 5-day forecast that displays the date - outstanding
 an icon representation of weather conditions - complete
 the temperature - complete
@@ -43,4 +72,5 @@ click on a city in the search history
 ```
 
 The following image demonstrates the application functionality:
+
 
